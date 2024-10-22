ea5331b5d0a0d855075e568619e38dd9eb5d3d22
mdoyle91 <mike.w.doyle91@gmail.com>
2024-10-22 17:41:10 -0400

    Some Front-End CUD

--- client/script.js
+++ client/script.js
@@ -12,7 +12,7 @@ const currentFees = document.querySelector(`#currentFees`);
 const feeRate = document.querySelector(`#feeRate`);
 const feeFrequency = document.querySelector(`#feeFrequency`);
 
-//Functions
+//Function to Pull Info Regarding Towed Cars
 
 searchButton.addEventListener(`click`, async () => {
   const searchTerm = searchBar.value.trim();
@@ -94,3 +94,87 @@ searchButton.addEventListener(`click`, async () => {
 //In Javascript I need:
 //Search Functionality-eventListener based on clicking search button
 //Dynamically created displays--create DIVs in html, and used CSS to give them a display of none, the click event will dynamically create the display (need a function for the display creation)
+
+//Front end CUD functionality--Utilized this source throughout for guidance in the code https://agirlcodes.medium.com/build-a-crud-todo-app-with-vanilla-javascript-and-fetch-api-44a664c0de52
+
+const localHostAddress = `http://localhost:3001/vehicles`;
+const newVehicleInput = document.querySelector(`#new-vehicle input`);
+let submitButton = document.querySelector(`#submit`);
+
+async function getVehicles() {
+  try {
+    const response = await fetch(localHostAddress);
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+async function createVehicle(data) {
+  try {
+    const response = await fetch(localHostAddress, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    console.log("Success:", result.message);
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+async function addVehicle() {
+  const data = { title: newVehicleInput.value };
+  await createVehicle(data);
+  displayVehicles();
+  newVehicleInput.value = "";
+}
+
+async function displayVehicles() {
+  const vehicleList = await getVehicles();
+  console.log(vehicleList);
+  let vehicleListContainer = document.querySelector(`#vehicles`);
+
+  // if (vehicleList.length == 0) {
+  if (!vehicleList) {
+    vehicleListContainer.innerHTML += `
+    <div class="vehicle">
+      <span> Loading Vehicles... </span>
+    </div>
+    `;
+  } else {
+    vehicleList.forEach((vehicle) => {
+      vehicleListContainer.innerHTML += `
+      <div class="vehicle">
+        <span>${vehicle.license_plate}</span>  
+        
+        <div class="actions">
+          <button class="edit">
+            <i class="fas fa-edit></i>
+          </button>
+          <button class="delete">
+          <i class="far fa-trash-alt"></i>
+          </button>
+        </div>
+      
+        </div>
+      `;
+    });
+  }
+}
+
+submitButton.addEventListener(`click`, () => addVehicle());
+
+displayVehicles();
+
+// const onload = async () => {
+//   await getVehicles();
+//   displayVehicles();
+// };
+
+// onload();
